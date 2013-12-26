ce0d65f12331e4cc2af524d7330acb9f24a414dd
Devasia Manuel <devasia1000@gmail.com>
2013-12-25 21:22:33 -0600

    weather_data class is complete

--- weather_data.py
+++ weather_data.py
@@ -1,7 +1,8 @@
 import urllib2
 import json
+import sys
 
-# WARNING: this class is slow. Use sparingly!
+# WARNING: grabbing weather data from OpenWeatherMap is slow. Use this class sparingly!
 
 # define a class called WeatherData
 class WeatherData:
@@ -9,27 +10,55 @@ class WeatherData:
     # the request URL
     url = "http://api.openweathermap.org/data/2.5/weather";
 
-    # constructor that accepts latitude and longitude and prints out weather data
+    # constructor that accepts latitude and longitude and gets weather data
     def __init__(self, latitude, longitude):
-    
+        
         # check that both latitude and longitude are passed in as floats
         latitude = float(latitude)
         longtitude = float(longitude)
 
         # perform the GET and store result
-        result = urllib2.urlopen("http://api.openweathermap.org/data/2.5/weather?lat="+str(latitude)+"&lon="+str(longitude)).read()
+        result = urllib2.urlopen(WeatherData.url+"?lat="+str(latitude)+"&lon="+str(longitude)).read()
         
-        # print out result
-        print result
-
         # parse the JSON result
-        data = json.loads(result)
+        self.data = json.loads(result)
 
-        # get relevant data from JSON string
-        temperature = data["main"]["temp"]
-        humidity = data["main"]["humidity"];
+        # get relevant data from JSON string. If data item cannot be found, set it to 'None'        
+        try:
+            self.cloud_cover = self.data["clouds"]["all"]; 
+        except KeyError:
+            self.cloud_cover = None
 
-        print temperature, humidity
+        try:    
+            self.temperature = self.data["main"]["temp"]
+        except KeyError:
+            self.temperature = None
+    
+        try:
+            self.humidity = self.data["main"]["humidity"]
+        except KeyError:
+            self.humidity = None
+    
+    # function to return cloud cover data
+    def getCloudCover(self):
+
+        return self.cloud_cover
+
+    # function to return temperature data. Temperature data is returned in Kelvib
+    def getTemperature(self):
+
+        return self.temperature
+
+    # function to return humidity data, Humidity data is return as a percentage
+    def getHumidity(self):
+
+        return self.humidity
+
+    # function to return all JSON data
+    def getAllJSONData(self):
+
+        return self.data
 
 # used to test our class        
 weather_data = WeatherData(12.3, 56.32)
+print weather_data.getCloudCover(), weather_data.getTemperature(), weather_data.getHumidity(), weather_data.getAllJSONData()
